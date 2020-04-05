982cb001e7977eed5ab4258ba2525c5bb9ed0ba6
Jessica Sills <jsills3110@gmail.com>
2020-04-05 11:04:19 -0700

    Initial commit.

---
+++ app/__init__.py
@@ -0,0 +1,40 @@
+# Third-party imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+# Local imports
+from config import app_config
+
+# DB variable initialization
+db = SQLAlchemy()
+
+# Login manager initialization
+login_manager = LoginManager()
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config = True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    
+    db.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
+    return app
---
+++ instance/config.py
@@ -0,0 +1,2 @@
+SECRET_KEY = 'secret-key'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dt_admin:dt2020@localhost/dreamteam_db'
