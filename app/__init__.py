06aefe6ff46c2975adb65fdf7315fc2f8bbd8309
Jessica Sills <jsills3110@gmail.com>
2020-04-05 16:09:05 -0700

    Made the links on the base.html file dynamic based on whether or not the user is logged in.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 # Local imports
 from config import app_config
@@ -18,6 +19,7 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     
+    Bootstrap(app)
     db.init_app(app)
 
     login_manager.init_app(app)
@@ -28,11 +30,11 @@ def create_app(config_name):
 
     from app import models
 
-    #from .admin import admin as admin_blueprint
-    #app.register_blueprint(admin_blueprint, url_prefix='/admin')
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-    #from .auth import auth as auth_blueprint
-    #app.register_blueprint(auth_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
